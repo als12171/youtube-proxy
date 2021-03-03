7ba8f75ffa852c76cdb7ebd2f047fc40b1a9133c
Andre Sousa <andre.sousa@bnieuropa.pt>
2021-03-03 17:40:58 +0000

    youtube proxy

--- api/web_player.js
+++ api/web_player.js
@@ -28,10 +28,40 @@ function fetch_target_id(req, res) {
           }
         });
       });
-      ytdl(url).pipe(writer);
+      ytdl(url, {quality: "lowestvideo"}).pipe(writer);
     }
   });
 }
+
+let starttime = 0;
+video.once("response", () => {
+  starttime = Date.now();
+});
+
+video.on("progress", (chunkLength, downloaded, total) => {
+  const percent = downloaded / total;
+  const downloadedSeconds = (Date.now() - starttime) / 1000;
+
+  const friedlyPercentage = Number((percent * 100).toFixed(2));
+  const downloadedMbs = Number((downloaded / 1024 / 1024).toFixed(2));
+  const totalInMbs = Number((total / 1024 / 1024).toFixed(2));
+  const estimatedLeft = parseInt(downloadedSeconds / percent - downloadedSeconds, 10);
+
+  const percentStr = `${friedlyPercentage}%`;
+  const downloadedMbStr = `${downloadedMbs}MB(s) of ${totalInMbs}MB(s)`;
+  const estimatedLeftStr = `${estimatedLeft} second(s)`;
+
+  if (downloaded !== total)
+  {
+	console.log(
+		`> ${percentStr} downloaded - ${downloadedMbStr} - estimated time left: ${estimatedLeftStr}`
+	  );
+  }
+  else{
+	  console.log(`✅ Video downloaded. It took: ${downloadedSeconds}second(s) for download process.`);
+  }
+
+});
 
 
 module.exports = function(app) {
